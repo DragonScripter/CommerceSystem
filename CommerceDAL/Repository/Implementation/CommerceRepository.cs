b51d26ac4b934a6fcbf636c4b9e0c6778776db73
Jude Ma <dragonscripter03@gmail.com>
2024-12-19 16:26:46 -0500

    Added the implementation with placeholder methods

---
+++ CommerceDAL/Repository/Implementation/CommerceRepository.cs
@@ -0,0 +1,43 @@
+﻿using CommerceDAL.Entities;
+using CommerceDAL.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceDAL.Repository.Implementation
+{
+    public class CommerceRepository<T> : IRepository<T> where T : CommerceEntity
+    {
+
+        public Task<List<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T?>> GetOne(Expression<Func<T, bool>> match)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetSome(Expression<Func<T, bool>> match)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<T> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Update> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
