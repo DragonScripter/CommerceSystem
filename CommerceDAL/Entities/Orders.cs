cb7c201f8c83d8536f0426c997bc1f02d6c31a92
Jude Ma <dragonscripter03@gmail.com>
2024-12-19 16:33:04 -0500

    added relationships and navigation for orders table

--- CommerceDAL/Entities/Orders.cs
+++ CommerceDAL/Entities/Orders.cs
@@ -8,8 +8,13 @@ namespace CommerceDAL.Entities
 {
     public class Orders : CommerceEntity
     {
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
+
+        public virtual Users Users { get; set; } = null!;
+        public virtual Product Product { get; set; } = null!;
     }
 }
