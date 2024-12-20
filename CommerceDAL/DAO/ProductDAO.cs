0292f654628fe78878a8d88c952f48e652d191d1
Jude Ma <dragonscripter03@gmail.com>
2024-12-20 14:14:30 -0500

    Created the product DAO get all method

---
+++ CommerceDAL/DAO/ProductDAO.cs
@@ -0,0 +1,38 @@
+﻿using CommerceDAL.Entities;
+using CommerceDAL.Repository.Implementation;
+using CommerceDAL.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceDAL.DAO
+{
+    public class ProductDAO
+    {
+        readonly IRepository<Product> _repo;
+        public ProductDAO()
+        {
+            _repo = new CommerceRepository<Product>();
+        }
+
+        public async Task<List<Product>> GetAll() 
+        {
+            List<Product> allProducts = new();
+            try
+            {
+                allProducts = await _repo.GetAll();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Problem in " + GetType().Name + " " +
+                MethodBase.GetCurrentMethod()!.Name + " " + ex.Message);
+                throw;
+            }
+            return allProducts;
+        }
+    }
+}
