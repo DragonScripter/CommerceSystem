bc064c64a612956b6c3c861216bb96290e64528a
Jude Ma <dragonscripter03@gmail.com>
2024-12-20 14:16:47 -0500

    Created the product view model for seperation of concerns

--- CommerceDAL/obj/Debug/net8.0/CommerceDAL.AssemblyInfo.cs
+++ CommerceDAL/obj/Debug/net8.0/CommerceDAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommerceDAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a31adc3908dd90688b4ff188b1c554a8fd11013e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0292f654628fe78878a8d88c952f48e652d191d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommerceDAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommerceDAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
