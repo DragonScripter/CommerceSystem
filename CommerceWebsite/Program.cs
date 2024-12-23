deed4e9912b45c86b03461358665abc82694558c
Jude Ma <dragonscripter03@gmail.com>
2024-12-23 18:25:47 -0500

    updated it so DI works, now just adding some helper methods for easier adding more services

--- CommerceWebsite/Program.cs
+++ CommerceWebsite/Program.cs
@@ -1,17 +1,37 @@
 using CommerceDAL;
+using CommerceDAL.DAO;
+using CommerceDAL.Entities;
+using CommerceDAL.Repository.Implementation;
+using CommerceDAL.Repository.Interface;
+using CommerceViewModels;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<CommerceContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+        options.UseSqlServer("Server=(localdb)\\ProjectModels;Database=CommerceDb;Trusted_Connection=True;"));
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowAll", policy =>
+        {
+            policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+    });
+//builder.Services.AddRepositories();
+//builder.Services.AddDAOs();
+//builder.Services.AddViewModels();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -21,6 +41,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseCors("AllowAll"); // Enable CORS for all controllers
 
 app.UseRouting();
