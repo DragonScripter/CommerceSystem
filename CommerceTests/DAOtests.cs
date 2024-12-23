bce479a8625ff65865b5f9f34b7fd495fc58d4de
Jude Ma <dragonscripter03@gmail.com>
2024-12-23 18:46:34 -0500

    updated it so tests can run now

--- CommerceTests/DAOtests.cs
+++ CommerceTests/DAOtests.cs
@@ -1,5 +1,9 @@
-﻿using CommerceDAL.DAO;
-using CommerceViewModels;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.InMemory;
+using CommerceDAL.DAO;
+using CommerceDAL;
+using CommerceDAL.Entities;
 namespace CommerceTests
 {
     public class DAOtests
@@ -7,17 +11,59 @@ namespace CommerceTests
         private readonly ProductDAO _pDAO;
         private readonly StocksDAO _sDAO;
 
-        public DAOtests(ProductDAO productDAO, StocksDAO stocksDAO) 
+        public DAOtests()
         {
-            _pDAO = productDAO;
-            _sDAO = stocksDAO;
+            // set the service
+            var services = new ServiceCollection();
+
+            // in mem database
+            services.AddDbContext<CommerceContext>(options =>
+                options.UseInMemoryDatabase("TestCommerceDb"));
+
+            // just adding them to service
+            services.AddScoped<ProductDAO>();
+            services.AddScoped<StocksDAO>();
+
+           
+            var serviceProvider = services.BuildServiceProvider();
+
+
+            _pDAO = serviceProvider.GetRequiredService<ProductDAO>();
+            _sDAO = serviceProvider.GetRequiredService<StocksDAO>();
+
+            // Seedind
+            SeedDatabase(serviceProvider);
         }
+
+
+        private void SeedDatabase(ServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<CommerceContext>();
+
+            context.Product.AddRange(new Product
+            {
+                Id = 1,
+                Name = "Product 1",
+                Description = "Sample product description",
+                Price = 100,
+                Timer = new byte[] { 1, 2, 3 }
+            });
+
+            context.Stocks.AddRange(new Stocks
+            {
+                Id = 1,
+                ProductId = 1,
+                Quanity = 50
+            });
+
+            context.SaveChanges();
+        }
+
         [Fact]
         public async Task GetAllTest()
         {
-            List<ProductViewModel> allProductsVms;
-            ProductViewModel vm = new(_pDAO, _sDAO);
-            allProductsVms = await vm.GetAll();
+
+            var allProductsVms = await _pDAO.GetAll();
             Assert.True(allProductsVms.Count > 0);
         }
     }
