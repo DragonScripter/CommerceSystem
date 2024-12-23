a92efb59910ebe62d98c4ed08f3e58c4af6a92b4
Jude Ma <dragonscripter03@gmail.com>
2024-12-23 18:35:09 -0500

    added the following constructor, and used the _pDAO and _sDAO to run the test

---
+++ CommerceTests/DAOtests.cs
@@ -0,0 +1,24 @@
+﻿using CommerceDAL.DAO;
+using CommerceViewModels;
+namespace CommerceTests
+{
+    public class DAOtests
+    {
+        private readonly ProductDAO _pDAO;
+        private readonly StocksDAO _sDAO;
+
+        public DAOtests(ProductDAO productDAO, StocksDAO stocksDAO) 
+        {
+            _pDAO = productDAO;
+            _sDAO = stocksDAO;
+        }
+        [Fact]
+        public async Task GetAllTest()
+        {
+            List<ProductViewModel> allProductsVms;
+            ProductViewModel vm = new(_pDAO, _sDAO);
+            allProductsVms = await vm.GetAll();
+            Assert.True(allProductsVms.Count > 0);
+        }
+    }
+}
