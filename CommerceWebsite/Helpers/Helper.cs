090b4559c2937d0ac9d5ff024d0957a9498956ac
Jude Ma <dragonscripter03@gmail.com>
2024-12-23 18:27:44 -0500

    created a helper folder and class for easier time in future to maintain or add more services

---
+++ CommerceWebsite/Helpers/Helper.cs
@@ -0,0 +1,29 @@
+﻿using CommerceDAL.DAO;
+using CommerceDAL.Entities;
+using CommerceDAL.Repository.Implementation;
+using CommerceDAL.Repository.Interface;
+using CommerceViewModels;
+
+namespace CommerceWebsite.Helpers
+{
+    public static class Helper
+    {
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository<Product>, CommerceRepository<Product>>();
+            services.AddScoped<IRepository<Stocks>, CommerceRepository<Stocks>>();
+            services.AddScoped<IRepository<Users>, CommerceRepository<Users>>();
+        }
+
+        public static void AddDAOs(this IServiceCollection services)
+        {
+            services.AddScoped<ProductDAO>();
+            services.AddScoped<StocksDAO>();
+        }
+
+        public static void AddViewModels(this IServiceCollection services)
+        {
+            services.AddScoped<ProductViewModel>();
+        }
+    }
+}
