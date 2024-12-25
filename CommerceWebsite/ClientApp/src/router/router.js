3aea3ae71a5a305fd4913b57223a010db7c962d8
Jude Ma <dragonscripter03@gmail.com>
2024-12-24 21:15:42 -0500

    created the router to route the different vues

---
+++ CommerceWebsite/ClientApp/src/router/router.js
@@ -0,0 +1,18 @@
+﻿import { createRouter, createWebHistory } from 'vue-router';
+
+import HomePage from '@/components/ProductDisplay.vue'; // Your components
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: HomePage,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(), // Enables HTML5 history mode
+    routes,
+});
+
+export default router;
