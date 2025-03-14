5180e80dd72f97cda45466b50ec3ca8f2e49d87e
Jude Ma <dragonscripter03@gmail.com>
2025-03-13 20:55:26 -0400

    finished setting up the jwt in program.cs

--- CommerceWebsite/Program.cs
+++ CommerceWebsite/Program.cs
@@ -5,10 +5,30 @@ using CommerceDAL.Repository.Implementation;
 using CommerceDAL.Repository.Interface;
 using CommerceViewModels;
 using CommerceWebsite.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Services.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecurityKey"]))
+        };
+    });
 
 builder.Services.AddDbContext<CommerceContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
