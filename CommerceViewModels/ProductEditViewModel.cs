311c6f82fdcb1904a03c2134e9e00153b18c4d0f
Jude Ma <dragonscripter03@gmail.com>
2025-04-28 13:18:01 -0400

    Created a view model for product edit

---
+++ CommerceViewModels/ProductEditViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+
+namespace CommerceViewModels
+{
+    public class ProductEditViewModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public IFormFile formFile { get; set; }
+        public int StockQuanity { get; set; }
+        public List<int> SelectedCategory { get; set; } = new List<int>();
+        public IEnumerable<SelectListItem> Categories { get; set; } = new List<SelectListItem>();
+    }
+}
